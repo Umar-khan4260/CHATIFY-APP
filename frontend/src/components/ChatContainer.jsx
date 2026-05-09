53ac0300e57979573be66569f5b4937938f959ed
umarKhan <umar68408@gmail.com>
2026-05-09 08:16:59 +0500

    chatpage layout setup

---
+++ frontend/src/components/ChatContainer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ChatContainer() {
+  return <div>chat containor</div>;
+}
+
+export default ChatContainer;
---
+++ frontend/src/components/ProfileHeader.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ProfileHeader() {
+  return <div>Profileheader</div>;
+}
+
+export default ProfileHeader;
